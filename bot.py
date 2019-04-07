10f20d2d6a445df69ba95bfa21bd827d97e81015
ThE KinG <43880032+TheKinG2149@users.noreply.github.com>
2019-04-07 17:52:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,5 +38,5 @@ logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
+bot.run(os.getenv('Token'))
 
-bot.run("", bot=True, reconnect=True)
